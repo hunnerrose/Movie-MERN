9e0a8af46e2d6708fe730ee0aaa3bba9e3407775
Fernando Estrada <Estradafr@yahoo.com>
2023-04-16 14:02:25 -0700

    created server.js

---
+++ API/server.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+
+// Middlewares and config
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Listen
+app.listen(9000, () => {
+  console.log(`LIVE ON PORT: 9000`);
+});
+
