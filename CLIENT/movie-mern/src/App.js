a539636601c2ef8ab1560d760134f8e74501a475
Miguel <mig256@outlook.com>
2023-04-17 12:45:40 -0700

    created useEffect function for our api, created components directory

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+import Gallery from "./components/gallery";
+
 function App() {
-  return <div></div>;
+  let [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/550?api_key=7b627fa55bf0652f8c45e9da6e8199d1&Title=%27`
+    )
+      .then((response) => response.json())
+      .then((data) => setSearch(data.title))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <Gallery search={search} />
+    </div>
+  );
 }
 
 export default App;
---
+++ CLIENT/movie-mern/src/components/gallery.js
@@ -0,0 +1,5 @@
+import "../index.css";
+
+export default function Gallery({ search }) {
+  return <div className="container">{search}</div>;
+}
