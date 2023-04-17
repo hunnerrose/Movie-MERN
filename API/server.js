294fc2d0e99f464a3db2cb77670e1243018c13fe
Fernando Estrada <Estradafr@yahoo.com>
2023-04-16 21:10:29 -0700

    completed controller for comments, integrated routes, connected db with our controller and migrations

--- API/server.js
+++ API/server.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to our movie api',
+  });
+});
+
 // Controllers
-const moviesController = require('./controllers/movies_controller');
-app.use('/movies', moviesController);
+const commentsController = require('./controllers/comments_controller');
+app.use('/comments', commentsController);
 
 // Listen
-app.listen(9000, () => {
-  console.log(`LIVE ON PORT: 9000`);
+app.listen(5999, () => {
+  console.log(`LIVE ON PORT:` + 5999);
 });
