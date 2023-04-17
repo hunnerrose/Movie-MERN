c0841afc50a70716e0ae652d57e5e99d70e295d1
Miguel <mig256@outlook.com>
2023-04-16 17:15:16 -0700

    saving server.js to my branch

--- API/server.js
+++ API/server.js
@@ -1,12 +1,12 @@
 // Dependencies
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-const { Sequelize } = require('sequelize');
+const cors = require("cors");
+const path = require("path");
+const { Sequelize } = require("sequelize");
 
 // Middlewares and config
-require('dotenv').config();
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,4 +15,3 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(9000, () => {
   console.log(`LIVE ON PORT: 9000`);
 });
-
