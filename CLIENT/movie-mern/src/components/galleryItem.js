5d557862b5908415459e6560aada81032846660e
Fernando Estrada <Estradafr@yahoo.com>
2023-04-18 20:13:23 -0700

    styled galleryItem with bootstrap

--- CLIENT/movie-mern/src/components/galleryItem.js
+++ CLIENT/movie-mern/src/components/galleryItem.js
@@ -22,24 +22,24 @@ export default function GalleryItem() {
     <ul className='d-flex flex-row flex-wrap'>
       {movies.map((movie) => (
         <Card
+          style={{ width: '18rem' }}
           border='secondary'
-          style={{ width: '15rem' }}
           key={movie.id}
           className='m-2'
         >
+          <Card.Img
+            variant='top'
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.title}
+          />
           <Card.Body>
             <Card.Title>{movie.title}</Card.Title>
-            <Card.Subtitle className='mt-2 text-muted'>
+            <Card.Subtitle className='text-muted mt-1'>
               {new Date(movie.release_date).toLocaleDateString(
                 'en-US',
                 dateOptions
               )}
             </Card.Subtitle>
-            <Card.Img
-              className='card-img mt-2 w-100'
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-            />
           </Card.Body>
         </Card>
       ))}
