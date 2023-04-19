9d9f99abc0bf491e58b51a312dc6fa9c231aa3ea
Roman M <rmarchen95@gmail.com>
2023-04-18 19:33:17 -0700

    added side bar

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { MovieContext } from "./components/movieContext";
 import SideBar from "./components/sideBar";
 import Gallery from "./components/gallery";
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 function App() {
   const [query, setQuery] = useState("");
@@ -24,13 +28,65 @@ function App() {
     }
   }, [query]);
 
+  //   <MovieContext.Provider value={{ movies }}>
+  //     <div className="appContainer">
+  //       <SideBar query={query} setQuery={setQuery} />
+  //       <Gallery />
+  //     </div>
+  // </MovieContext.Provider>
+  
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <MovieContext.Provider value={{ movies }}>
-      <div className="appContainer">
-        <SideBar query={query} setQuery={setQuery} />
-        <Gallery />
+  <div>
+    <SideNav
+      onSelect={(selected) => {
+        // Add your code here
+      }}
+    >
+      <SideNav.Toggle />
+      <SideNav.Nav defaultSelected="home">
+        <NavItem eventKey="home">
+          <NavIcon>
+            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+          </NavIcon>
+          <NavText>
+      
+      
+      <input
+        className="input"
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+      />
+          </NavText>
+        </NavItem>
+        <NavItem eventKey="charts">
+          <NavIcon>
+            <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+          </NavIcon>
+          <NavText>
+                Movies
+            </NavText>
+            <NavItem eventKey="charts/recentlyAdded">
+                <NavText>
+                    Recently Added
+                </NavText>
+            </NavItem>
+            <NavItem eventKey="charts/upcoming">
+                <NavText>
+                    Popular Movies
+                </NavText>
+          </NavItem>
+        </NavItem>
+      </SideNav.Nav>
+    </SideNav>
+      <Gallery />
       </div>
-    </MovieContext.Provider>
+      </MovieContext.Provider>
   );
 }
 
