2319478283a6f978aeb7cdbe13e2204f26221ba8
Fernando Estrada <Estradafr@yahoo.com>
2023-04-18 20:43:16 -0700

    styled galleryItem with bootstrap

--- CLIENT/movie-mern/src/components/sideBar.js
+++ CLIENT/movie-mern/src/components/sideBar.js
@@ -1,4 +1,5 @@
-import "../index.css";
+import '../index.css';
+import { FloatingLabel, Form, Row, Col } from 'react-bootstrap';
 
 export default function SideBar({ query, setQuery }) {
   const handleInputChange = (event) => {
@@ -6,14 +7,32 @@ export default function SideBar({ query, setQuery }) {
   };
 
   return (
-    <label className="sidebar">
-      Search for a movie:
-      <input
-        className="input"
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-      />
-    </label>
+    // <label className='sidebar'>
+    //   Search for a movie:
+    //   <input
+    //     className='input'
+    //     type='text'
+    //     value={query}
+    //     onChange={handleInputChange}
+    //   />
+    // </label>
+    <div className='sidebar'>
+      <Row className='g-2'>
+        <Col md>
+          <FloatingLabel
+            controlId='floatingInputGrid'
+            label='Search for a movie'
+            className='position-fixed'
+          >
+            <Form.Control
+              type='text'
+              value={query}
+              onChange={handleInputChange}
+              placeholder='The Batman'
+            />
+          </FloatingLabel>
+        </Col>
+      </Row>
+    </div>
   );
 }
