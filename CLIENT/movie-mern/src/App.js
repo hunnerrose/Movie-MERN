21cdd28a52c0a15f207c0824aa0ea166ea597d4b
Fernando Estrada <Estradafr@yahoo.com>
2023-04-18 18:33:07 -0700

    updated the fetch and created an API var

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -1,17 +1,18 @@
-import { useState, useEffect } from "react";
-import { MovieContext } from "./components/movieContext";
-import SideBar from "./components/sideBar";
-import Gallery from "./components/gallery";
+import { useState, useEffect } from 'react';
+import { MovieContext } from './components/movieContext';
+import SideBar from './components/sideBar';
+import Gallery from './components/gallery';
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
+  const API_URL =
+    'https://api.themoviedb.org/3/search/movie?api_key=7b627fa55bf0652f8c45e9da6e8199d1';
+
   useEffect(() => {
-    if (query !== "") {
-      fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=7b627fa55bf0652f8c45e9da6e8199d1&query=${query}`
-      )
+    if (query !== '') {
+      fetch(`${API_URL}&query=${query}`)
         .then((response) => {
           // console.log(response);
           return response.json();
@@ -26,8 +27,11 @@ function App() {
 
   return (
     <MovieContext.Provider value={{ movies }}>
-      <div className="appContainer">
-        <SideBar query={query} setQuery={setQuery} />
+      <div className='appContainer'>
+        <SideBar
+          query={query}
+          setQuery={setQuery}
+        />
         <Gallery />
       </div>
     </MovieContext.Provider>
