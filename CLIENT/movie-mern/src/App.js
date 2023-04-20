988099416475911bed18e130b6a31139aadaafdc
Fernando Estrada <Estradafr@yahoo.com>
2023-04-20 10:27:07 -0700

    Eddited useEffect to pull featured movies on load and allow for a query search

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -1,20 +1,28 @@
 import { useState, useEffect } from 'react';
 import { MovieContext } from './components/movieContext';
 
-import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery';
-import SideBar from './components/SideBar';
+import SideBar from './components/sideBar';
 
 function App() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
-  const API_URL =
-    'https://api.themoviedb.org/3/search/movie?api_key=7b627fa55bf0652f8c45e9da6e8199d1';
+  const API_URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
+  const FEAT_API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=';
+  const API_KEY = '7b627fa55bf0652f8c45e9da6e8199d1';
 
   useEffect(() => {
     if (query !== '') {
-      fetch(`${API_URL}&query=${query}`)
+      fetch(`${API_URL}${API_KEY}&query=${query}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setMovies(data.results);
+        });
+    } else {
+      fetch(`${FEAT_API_URL}${API_KEY}&sort_by=popularity.desc`)
         .then((response) => {
           return response.json();
         })
