07ab49e3e61ba094325449343119238b590f5f5c
Fernando Estrada <Estradafr@yahoo.com>
2023-04-20 12:32:02 -0700

    Eddited the main page to load the default movie with some info!

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { MovieContext } from './components/movieContext';
+import { InputText } from 'primereact/inputtext';
 
 import Gallery from './components/Gallery';
 import SideBar from './components/sideBar';
@@ -7,28 +8,41 @@ import SideBar from './components/sideBar';
 function App() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState([]);
 
   const API_URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
   const FEAT_API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=';
   const API_KEY = '7b627fa55bf0652f8c45e9da6e8199d1';
+  const BACKDROP_IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
+
+  const dateOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'UTC',
+  };
+
+  async function fetchFeaturedMovies() {
+    const response = await fetch(
+      `${FEAT_API_URL}${API_KEY}&sort_by=popularity.desc`
+    );
+    const data = await response.json();
+    setMovies(data.results);
+    setSelectedMovie(data.results[0]);
+  }
+
+  async function fetchAMovie() {
+    const response = await fetch(`${API_URL}${API_KEY}&query=${query}`);
+    const data = await response.json();
+    setMovies(data.results);
+    setSelectedMovie(data.results[0]);
+  }
 
   useEffect(() => {
     if (query !== '') {
-      fetch(`${API_URL}${API_KEY}&query=${query}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          setMovies(data.results);
-        });
+      fetchAMovie();
     } else {
-      fetch(`${FEAT_API_URL}${API_KEY}&sort_by=popularity.desc`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          setMovies(data.results);
-        });
+      fetchFeaturedMovies();
     }
   }, [query]);
 
@@ -39,7 +53,48 @@ function App() {
           query={query}
           setQuery={setQuery}
         />
-        <Gallery />
+
+        <header
+          id='header'
+          className='mb-2 mx-5'
+        >
+          <div className='d-flex align-items-center justify-content-between'>
+            <h4>SHMOVIE FANATICS</h4>
+            <span className='p-float-label p-input-icon-left'>
+              <i className='pi pi-search' />
+              <InputText
+                id='lefticon'
+                value={query}
+                setQuery={setQuery}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <label htmlFor='lefticon'>Search for a movie</label>
+            </span>
+          </div>
+        </header>
+
+        <div
+          id='movie-display'
+          className='movie-display'
+          style={{
+            backgroundImage: `url(${BACKDROP_IMG_PATH}${selectedMovie.backdrop_path})`,
+          }}
+        >
+          <div className='movie-content'>
+            <h1 className='movie-content-title'>{selectedMovie.title}</h1>
+            <p className='text-white'>{selectedMovie.overview}</p>
+            <p className='text-white'>
+              {new Date(selectedMovie.release_date).toLocaleDateString(
+                'en-US',
+                dateOptions
+              )}
+            </p>
+          </div>
+        </div>
+
+        <div id='gallery'>
+          <Gallery />
+        </div>
       </MovieContext.Provider>
     </div>
   );
--- CLIENT/movie-mern/src/components/galleryItem.js
+++ CLIENT/movie-mern/src/components/galleryItem.js
@@ -19,17 +19,17 @@ export default function GalleryItem() {
     timeZone: 'UTC',
   };
 
-  const expandedStyle = {
-    //   width: "80vw",
-    //   height: "20vh",
-    //   border: "1px solid black",
-    //   margin: "2px",
-    //   // backgroundImage: https://image.tmdb.org/t/p/w154/${movie.poster_path},
-    //   backgroundRepeat: "no-repeat",
-    //   backgroundSize: "cover",
-    fontSize: '5rem',
-    color: 'red',
-  };
+  // const expandedStyle = {
+  //   //   width: "80vw",
+  //   //   height: "20vh",
+  //   //   border: "1px solid black",
+  //   //   margin: "2px",
+  //   //   // backgroundImage: https://image.tmdb.org/t/p/w154/${movie.poster_path},
+  //   //   backgroundRepeat: "no-repeat",
+  //   //   backgroundSize: "cover",
+  //   // fontSize: '5rem',
+  //   // color: 'red',
+  // };
 
   // const expandedView = ({ movies }) => (
   //   <div className={expandedStyle}>
@@ -38,18 +38,50 @@ export default function GalleryItem() {
   // );
 
   // move to its in js file
-  const ExpandedView = () => (
-    <div className={expandedStyle}>
-      <h3>{movies[0].title}</h3>
-      {/* just an example, replace with the correct movie title */}
-    </div>
-  );
+  // const ExpandedView = () => (
+  //   <div className={expandedStyle}>
+  //     <h3>{movies[0].title}</h3>
+  //     {/* just an example, replace with the correct movie title */}
+  //   </div>
+  // );
 
-  const card = (
+  // const card = (
+  // <ul className='d-flex flex-row flex-wrap'>
+  //   {movies.map((movie) => (
+  //     <Card
+  //       style={{ width: '13rem' }}
+  //       border='secondary'
+  //       key={movie.id}
+  //       className='m-2'
+  //     >
+  //       <Card.Img
+  //         variant='top'
+  //         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+  //         alt={movie.title}
+  //       />
+  //       <Card.Body>
+  //         <Card.Title>{movie.title}</Card.Title>
+  //         <Card.Subtitle className='text-muted mt-1'>
+  //           {new Date(movie.release_date).toLocaleDateString(
+  //             'en-US',
+  //             dateOptions
+  //           )}
+  //         </Card.Subtitle>
+  //       </Card.Body>
+  //     </Card>
+  //   ))}
+  // </ul>
+  // );
+
+  // const handleClick = () => {
+  //   setIsExpanded(!isExpanded);
+  //   console.log(isExpanded);
+  // };
+  return (
     <ul className='d-flex flex-row flex-wrap'>
       {movies.map((movie) => (
         <Card
-          style={{ width: '13rem' }}
+          style={{ width: '10rem' }}
           border='secondary'
           key={movie.id}
           className='m-2'
@@ -71,13 +103,5 @@ export default function GalleryItem() {
         </Card>
       ))}
     </ul>
-  );
-
-  const handleClick = () => {
-    setIsExpanded(!isExpanded);
-    console.log(isExpanded);
-  };
-  return (
-    <div onClick={handleClick}>{isExpanded ? <ExpandedView /> : card}</div>
   );
 }
