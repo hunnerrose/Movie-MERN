c90d99e51900384e892ce8eb42df730a4473b145
Miguel <mig256@outlook.com>
2023-04-20 19:14:11 -0700

    na

--- CLIENT/movie-mern/src/App.js
+++ CLIENT/movie-mern/src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   //Movie data from API
-    let [movies, setMovies] = useState([])
+  let [movies, setMovies] = useState([]);
 
-  return (
-    <div>
-  
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
