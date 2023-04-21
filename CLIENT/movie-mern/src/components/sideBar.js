f38da48d20f21da80fe6497d520f22e435a3836f
Miguel <mig256@outlook.com>
2023-04-20 18:56:17 -0700

    changed font color, refactored code in sideBar.js

--- CLIENT/movie-mern/src/components/sideBar.js
+++ CLIENT/movie-mern/src/components/sideBar.js
@@ -5,7 +5,7 @@ import "primeicons/primeicons.css";
 import React, { useState } from "react";
 import { Sidebar } from "primereact/sidebar";
 import { Button } from "primereact/button";
-import SearchBar from "./SearchBar";
+// import SearchBar from "./SearchBar";
 import "../index.css";
 import { InputText } from "primereact/inputtext";
 
@@ -22,7 +22,7 @@ export default function SideBar({ query, setQuery }) {
         }}
       >
         <h3 className="mb-3">SHMOVIE FANATICS</h3>
-        <SearchBar query={query} setQuery={setQuery} />
+        {/*<SearchBar query={query} setQuery={setQuery} />*/}
         <h3 className="mb-4 text-white">SHMOVIE FANATICS</h3>
         <span className="p-float-label p-input-icon-left">
           <i className="pi pi-search" />
