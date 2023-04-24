92f35a273ad92e61574d23a0c222753bf3e030f9
Fernando Estrada <Estradafr@yahoo.com>
2023-04-24 16:42:33 -0700

    Styled the galleryItem to have a average rating

--- CLIENT/movie-mern/src/components/galleryItem.js
+++ CLIENT/movie-mern/src/components/galleryItem.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { MovieContext } from "../context/movieContext";
-import "../index.css";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { MovieContext } from '../context/movieContext';
+import { AiOutlineStar } from 'react-icons/ai';
+import '../index.css';
 
 export default function GalleryItem() {
   const { movies } = useContext(MovieContext);
@@ -11,33 +12,39 @@ export default function GalleryItem() {
   }
 
   const dateOptions = {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    timeZone: "UTC",
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'UTC',
   };
 
   return (
-    <ul className="list-unstyled d-flex flex-wrap justify-content-center align-items-stretch">
+    <ul className='list-unstyled d-flex flex-wrap justify-content-center align-items-stretch'>
       {movies.map((movie) => (
-        <div className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 p-1">
-          <div className="card bg-dark text-white border-secondary d-flex flex-column h-100">
+        <div className='col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 p-1'>
+          <div className='card bg-dark text-white border-secondary d-flex flex-column h-100'>
             <img
-              className="card-img-top flex-grow-1"
+              className='card-img-top flex-grow-1'
               src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
               alt={movie?.title}
             />
-            <div className="card-body text-center">
-              <h5 className="card-title text-white">{movie?.title}</h5>
-              <p className="card-text text-secondary">
+            <div className='card-body text-center flex flex-col justify-center'>
+              <h5 className='card-title text-white'>{movie?.title}</h5>
+              <p className='card-text text-secondary'>
                 {new Date(movie?.release_date).toLocaleDateString(
-                  "en-US",
+                  'en-US',
                   dateOptions
                 )}
               </p>
+              <p className='card-text text-secondary flex items-center justify-center'>
+                <span className='mr-1 mt-[2px] text-yellow-500'>
+                  <AiOutlineStar />
+                </span>
+                {movie?.vote_average} / 10
+              </p>
               <Link to={`/movies/${movie?.id}`}>
                 <button
-                  className="btn btn-outline-light mt-2"
+                  className='btn btn-outline-light mt-2'
                   onClick={HandleClick}
                 >
                   View More
