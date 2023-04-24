07b5af915e50561da9b5743e4fccf9f79da5e9dc
Fernando Estrada <Estradafr@yahoo.com>
2023-04-24 05:14:24 -0700

    added ALOT of styling. Please `npm i` to grab everything. Check out the app and see if there are any bugs please. Also: The buttons on sidebar work now AND the app has a responsive view!

--- CLIENT/movie-mern/src/components/galleryItem.js
+++ CLIENT/movie-mern/src/components/galleryItem.js
@@ -35,18 +35,18 @@ export default function GalleryItem() {
   };
 
   return (
-    <ul class='list-unstyled d-flex flex-wrap justify-content-center align-items-stretch'>
+    <ul className='list-unstyled d-flex flex-wrap justify-content-center align-items-stretch'>
       {movies.map((movie) => (
-        <div class='col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 p-1'>
-          <div class='card bg-dark text-white border-secondary d-flex flex-column h-100'>
+        <div className='col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 p-1'>
+          <div className='card bg-dark text-white border-secondary d-flex flex-column h-100'>
             <img
-              class='card-img-top flex-grow-1'
+              className='card-img-top flex-grow-1'
               src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
               alt={movie?.title}
             />
-            <div class='card-body text-center'>
-              <h5 class='card-title text-white'>{movie?.title}</h5>
-              <p class='card-text text-secondary'>
+            <div className='card-body text-center'>
+              <h5 className='card-title text-white'>{movie?.title}</h5>
+              <p className='card-text text-secondary'>
                 {new Date(movie?.release_date).toLocaleDateString(
                   'en-US',
                   dateOptions
@@ -54,7 +54,7 @@ export default function GalleryItem() {
               </p>
               <Link to={`/movies/${movie?.id}`}>
                 <button
-                  class='btn btn-outline-light mt-2'
+                  className='btn btn-outline-light mt-2'
                   onClick={HandleClick}
                 >
                   View More
