f0b67b22a63c468e1fa40036fbea4e085dd0f58a
Fernando Estrada <Estradafr@yahoo.com>
2023-04-24 04:40:58 -0700

    added ALOT of styling. Please `npm i` to grab everything. Check out the app and see if there are any bugs please. Also: The buttons on sidebar work now.

--- CLIENT/movie-mern/src/components/MovieView.js
+++ CLIENT/movie-mern/src/components/MovieView.js
@@ -73,41 +73,39 @@ export default function MovieView({ movies }) {
               )}
             </div>
             {/* <!-- Movie genres--> */}
-            <div className='text-center'>
-              {' '}
-              <a href='#!'>
-                <ul>
-                  {movie?.genres?.map((genre) => (
-                    <li
-                      className='badge
-                    bg-secondary
-                    text-decoration-none
-                    link-light
-                    mx-5'
-                      key={genre.id}
-                    >
-                      {genre.name}
-                    </li>
-                  ))}
-                </ul>
-              </a>
-            </div>
-            <div style={{ textAlign: 'center' }}>
-              {movie?.vote_average !== 0 && (
-                <h2
-                  style={{
-                    color: 'gold',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
+            <div className='flex flex-col items-center justify-center'>
+              <div>
+                <a
+                  href='#!'
+                  className='no-underline'
                 >
-                  <span className='p-2'>
-                    <FaStar />
-                  </span>
-                  {movie?.vote_average?.toFixed(2)}
-                </h2>
-              )}
+                  <ul className='flex flex-wrap justify-center'>
+                    {movie?.genres?.map((genre) => (
+                      <li
+                        className={`px-2 py-1 mr-2 bg-secondary text-light rounded-md ${
+                          movie?.genres?.length < 2 ? 'mx-auto' : ''
+                        }`}
+                        key={genre.id}
+                      >
+                        {genre.name}
+                      </li>
+                    ))}
+                  </ul>
+                </a>
+              </div>
+              <div>
+                {movie?.vote_average !== 0 && (
+                  <h2
+                    className='flex items-center'
+                    style={{ color: 'gold' }}
+                  >
+                    <span className='p-2'>
+                      <FaStar />
+                    </span>
+                    {movie?.vote_average?.toFixed(2)}
+                  </h2>
+                )}
+              </div>
             </div>
           </header>
         </div>
@@ -119,7 +117,7 @@ export default function MovieView({ movies }) {
               <div class='flex-container'>
                 <figure className='mb-4 flex-child'>
                   <img
-                    src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
                     alt={movie?.title}
                     className='movie-img'
                   />
@@ -129,73 +127,93 @@ export default function MovieView({ movies }) {
           </div>
           <div className='col-lg-1'></div>
           <div className='col-lg-7'>
-            {/* Fernando, can you figure out how we can display the multiple genres for each movie? I kept getting errors when I put movie.genres, b/c the genres portion is an array of objects. */}
             {/* <!-- Genres section--> */}
             <div className='card mb-4'>
               <div className='card-header'>Genres</div>
-              <div className='card-body'>
-                <ul className='list-unstyled mb-0 text-center'>
-                  <li>
-                    <a
-                      href='#!'
-                      className='text-decoration-none link-dark'
-                    >
-                      {movie?.genres?.[0]?.name}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href='#!'
-                      className='text-decoration-none link-dark'
-                    >
-                      {movie?.genres?.[1]?.name}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href='#!'
-                      className='text-decoration-none link-dark'
-                    >
-                      {movie?.genres?.[2]?.name}
-                    </a>
-                  </li>
-                </ul>
+              <div className='card-body text-center'>
+                <table className='table table-borderless'>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <a
+                          href='#!'
+                          className='text-decoration-none link-dark'
+                        >
+                          <b>{movie?.genres?.[0]?.name}</b>
+                        </a>
+                      </td>
+                      <td>
+                        <a
+                          href='#!'
+                          className='text-decoration-none link-dark'
+                        >
+                          <b>{movie?.genres?.[1]?.name}</b>
+                        </a>
+                      </td>
+                      <td>
+                        <a
+                          href='#!'
+                          className='text-decoration-none link-dark'
+                        >
+                          <b>{movie?.genres?.[2]?.name}</b>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
+
             {/* <!-- Production Info Section--> */}
             <div className='card mb-4'>
               <div className='card-header'>Production Info</div>
               <div className='card-body'>
-                <ul className='p-0'>
-                  <li>
-                    <b>Budget: </b>
-                    {movie?.budget === 0 ? (
-                      <text>Not Available</text>
-                    ) : (
-                      <text> {'$' + movie?.budget?.toLocaleString()}</text>
-                    )}
-                  </li>
-                  <li>
-                    <b>Revenue: </b>
-                    {movie?.revenue === 0 ? (
-                      <text>Not Available</text>
-                    ) : (
-                      <text> {'$' + movie?.revenue?.toLocaleString()}</text>
-                    )}
-                  </li>
-                  <li>
-                    <b>Production Companies: </b>
-                    {movie?.production_companies?.[0]?.name} |{' '}
-                    {movie?.production_companies?.[1]?.name}
-                  </li>
-                  <li>
-                    {' '}
-                    <b>Run Time: </b>
-                    {convertRuntime(movie?.runtime)}
-                  </li>
-                </ul>
+                <table className='table table-borderless'>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <b>Budget:</b>
+                      </td>
+                      <td>
+                        {movie?.budget === 0 ? (
+                          <text>Not Available</text>
+                        ) : (
+                          <text>{'$' + movie?.budget?.toLocaleString()}</text>
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Revenue:</b>
+                      </td>
+                      <td>
+                        {movie?.revenue === 0 ? (
+                          <text>Not Available</text>
+                        ) : (
+                          <text>{'$' + movie?.revenue?.toLocaleString()}</text>
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Production Companies:</b>
+                      </td>
+                      <td>
+                        {movie?.production_companies?.[0]?.name} |{' '}
+                        {movie?.production_companies?.[1]?.name}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Run Time:</b>
+                      </td>
+                      <td>{convertRuntime(movie?.runtime)}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
+
             {/* <!-- Movie overview section --> */}
             <div className='card mb-4'>
               <div className='card-header'>Description</div>
