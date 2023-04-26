3dfd7de044bb783e9d103da382719cbd67549589
Roman M <rmarchen95@gmail.com>
2023-04-25 18:09:05 -0700

    updated comments to communicate with api
    facing error not communicating with back end

--- CLIENT/movie-mern/src/components/comments.js
+++ CLIENT/movie-mern/src/components/comments.js
@@ -1,19 +1,94 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// function Comments() {
+//   const [comments, setComments] = useState([]);
+//   const [newComment, setNewComment] = useState("");
+
+//   const handleSubmit = (e) => {
+//     fetch.post
+//     e.preventDefault();
+//     setComments([...comments, newComment]);
+//     setNewComment("");
+//   };
+
+//   const handleDelete = (index) => {
+//     const newComments = [...comments];
+//     newComments.splice(index, 1);
+//     setComments(newComments);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={newComment}
+//           onChange={(e) => setNewComment(e.target.value)}
+//         />
+//         <button type="submit">Add Comment</button>
+//       </form>
+//       <ul>
+//         {comments.map((comment, index) => (
+//           <li key={index}>
+//             {comment}
+//             <button onClick={() => handleDelete(index)}>Delete Comment</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Comments;
+
+import React, { useState, useEffect } from "react";
 
 function Comments() {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
+  const API_BASE = "https://rqygsodbcvrutuezcjts.supabase.co/";
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    fetch(API_BASE)
+      .then((response) => response.json())
+      .then((data) => setComments(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setComments([...comments, newComment]);
-    setNewComment("");
+    try {
+      const response = await fetch(API_BASE, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ comment: newComment }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to add comment.");
+      }
+      const data = await response.json();
+      setComments([...comments, data.comment]);
+      setNewComment("");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleDelete = (index) => {
-    const newComments = [...comments];
-    newComments.splice(index, 1);
-    setComments(newComments);
+  const handleDelete = async (index) => {
+    try {
+      const response = await fetch(`${API_BASE}${index}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete comment.");
+      }
+      const newComments = comments.filter((comment, i) => i !== index);
+      setComments(newComments);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -29,7 +104,7 @@ function Comments() {
       <ul>
         {comments.map((comment, index) => (
           <li key={index}>
-            {comment}
+            {comment.text}
             <button onClick={() => handleDelete(index)}>Delete Comment</button>
           </li>
         ))}
