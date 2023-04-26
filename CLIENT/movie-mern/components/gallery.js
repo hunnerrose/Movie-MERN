e25b92c179291de8b89369bd8f16a6e6798a6a79
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-25 23:22:56 -0700

    small changes

--- CLIENT/movie-mern/components/gallery.js
+++
@@ -1,7 +0,0 @@
-export function Gallery () {
-    return (
-        <div>
-            
-        </div>
-    );
-}
--- CLIENT/movie-mern/components/galleryitem.js
+++
@@ -1,9 +0,0 @@
-export function GalleryItem ({ movie }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-
-
-    return (
-        <div>
-            <img src={movie.poster_path} alt="" />
-        </div>
-};
